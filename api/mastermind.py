1b028a2c115d072fa2940a7e129a5c947ad9a270
Jeremy Su <70295411+bulletchicken@users.noreply.github.com>
2024-11-28 11:42:20 -0500

    updating libraries

--- api/mastermind.py
+++ api/mastermind.py
@@ -13,6 +13,7 @@ BUCKET_NAME = "quantobucket"
 # Initialize S3 Client
 s3_client = boto3.client(
     "s3",
+    region_name='us-west-2',
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"), 
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY") 
 )
